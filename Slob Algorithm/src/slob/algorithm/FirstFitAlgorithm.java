429864af133bbd4ccf42586acb55132a7025b1cd
saadhere <38932963+saadhere@users.noreply.github.com>
2018-05-03 11:09:17 +0500

    FirstFitAlgorithm implementation
    The basic algo to be put in

--- Slob Algorithm/src/slob/algorithm/FirstFitAlgorithm.java
+++ Slob Algorithm/src/slob/algorithm/FirstFitAlgorithm.java
@@ -1,44 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package slob.algorithm;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Comparator;
 
-/**
- *
- * @author somanbaqai Things to do: also give column about fragmentation
- */
 public class FirstFitAlgorithm {
-
-    public static void FirstFit(int[] block, ArrayList<Long> MemoryBlockSize, ArrayList<SingleProcessNode> ProcessSize) {
-        int blockCount = 0;
-        for (int i = 0; i < ProcessSize.size(); i++) {
+    public static void FirstFit(int[] block, ArrayList<Long> MemBSize, ArrayList<SingleProcessNode> ProcessSize) {
+        int blockNo = 0;
+        for (int i = 0; i < ProcessSize.size(); i++) { // The basic algorithm to implement first first algo starts here
             Long size = ProcessSize.get(i).getSize();
-
-            for (int j = 0; j < MemoryBlockSize.size(); j++) {
-
-                if (MemoryBlockSize.get(j) >= size) {
-                    Long newBlock = MemoryBlockSize.get(j) - size;
-                    MemoryBlockSize.add(j, newBlock);
-                    MemoryBlockSize.remove(j + 1);
-
-                    ProcessSize.get(i).setFragmentation(newBlock);
-                    block[blockCount] = j + 1;
-                    blockCount++;
-
-                    break;
-
+            for (int j = 0; j < MemBSize.size(); j++) {
+                if (MemBSize.get(j) >= size) {
+                    Long newBlock = MemBSize.get(j) - size;
+                    MemBSize.add(j, newBlock);              // Allocating memory to the process
+                    MemBSize.remove(j + 1);
+                    ProcessSize.get(i).setFragmentation(newBlock);      // Fragmentation update
+                    block[blockNo] = j + 1;
+                    blockNo++;
+                    break; // Loop ends here
                 }
-
             }
-
         }
     }
-
 }
